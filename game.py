9ef912b96d0384e5fb1dff6a8d3c569ccd4be4c7
Aimored <alexpervyakov2@gmail.com>
2024-05-08 16:58:22 +0300

    Update game.py

--- game.py
+++ game.py
@@ -9,38 +9,27 @@ ASSETS_PATH = OUTPUT_PATH / Path(r"assets game\frame0")
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 window = Tk()
-
 window.geometry("1920x1080")
+
 window.configure(bg = "#FFFFFF")
 BASE_DELAY = 3000
-BASE_SPEED = 10 
+BASE_SPEED = 10
 BACKGROUND_SPEED = 1
-GRAVITY = 1
+GRAVITY = 0.9
 INITIAL_VELOCITY = 29
 is_jumping = False
 vertical_velocity = 0
-
-canvas = Canvas(
-    window,
-    bg = "#FFFFFF",
-    height = 1080,
-    width = 2483,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
-)
+paused = False 
+canvas = Canvas(window,bg = "#FFFFFF",height = 1080,width = 2483,bd = 0, highlightthickness = 0, relief = "ridge")
 
 image_image_1 = PhotoImage(file=relative_to_assets("image_1.png"))
-# Create two instances of the image (for seamless looping)
 image_1 = canvas.create_image(1241.0, 540.0, image=image_image_1)
-image_2 = canvas.create_image(3723.0, 540.0, image=image_image_1)  # This should be initially off-screen
+image_2 = canvas.create_image(3723.0, 540.0, image=image_image_1)
 
 def move_background():
-    # Increase speed based on krest_count, capped at 15
     speed_increment = min(krest_count, 10) + 0.1
     canvas.move(image_1, BACKGROUND_SPEED + speed_increment, 0)
     canvas.move(image_2, BACKGROUND_SPEED + speed_increment, 0)
-    
     x1, y1 = canvas.coords(image_1)
     x2, y2 = canvas.coords(image_2)
     image_width = image_image_1.width()
@@ -50,7 +39,7 @@ def move_background():
     if x2 >= canvas.winfo_width() + image_width / 2:
         canvas.coords(image_2, x1 - image_width, 540.0)
     
-    window.after(10, move_background) # тут
+    window.after(1, move_background)
     
 
 canvas.place(x = 0, y = 0)
@@ -73,37 +62,27 @@ def jump(event):
 def animate_jump():
     global is_jumping, vertical_velocity
     if is_jumping:
-        # Calculate new position
         y_position = canvas.coords(pers)[1] - vertical_velocity
         canvas.coords(pers, 1561.0, y_position)
         
-        # Update velocity
         vertical_velocity -= GRAVITY
         
-        # Check if `pers` has landed
         if y_position >= 724.0:
             canvas.coords(pers, 1561.0, 724.0)
             is_jumping = False
             vertical_velocity = 0
         else:
-            window.after(10, animate_jump)  # Schedule next frame
+            window.after(10, animate_jump)  
 
-paused = False  # Флаг для отслеживания состояния паузы
+
 
 
 def create_and_move_krest():
     if not paused:
-        # Generate a random y-coordinate between 540 and 1080
         y_coord = random.choice([random.randint(340, 430), random.randint(700, 800)])  
-
-        # Create the krest image at the left edge of the screen
         krest = canvas.create_image(0, y_coord, image=image_image_3)
         move_krest(krest)
-
-        # Calculate the delay, decreasing it by 500ms for each krest_count increment until krest_count reaches 15
         delay = max(500, 3000 - 75 * min(krest_count, 20))
-        
-        # Reschedule the creation of krest with the calculated delay
         window.after(delay, create_and_move_krest)
 
 krest_count_label = Label(window, text="пройдено: 0", bg="#FFFFFF", fg="#800080", font=("Helvetica", 16))
@@ -113,14 +92,13 @@ def move_krest(krest):
     global is_jumping, paused, krest_count
     if paused:
         return
-    # Increase speed based on krest_count, capped at 15
-    speed_increment = min(krest_count, 25) + 0.5 # тут
+    speed_increment = min(krest_count, 25) + 0.5 
     canvas.move(krest, BASE_SPEED + speed_increment, 0)
     krest_bbox = canvas.bbox(krest)
     pers_bbox = canvas.bbox(pers)
 
     krest_rect = [krest_bbox[0], krest_bbox[1], krest_bbox[2], krest_bbox[3]]
-    pers_rect = [pers_bbox[0], pers_bbox[1], pers_bbox[2], pers_bbox[3]]
+    pers_rect = [pers_bbox[0], pers_bbox[1], pers_bbox[2]-80, pers_bbox[3]]
 
     if check_collision(krest_rect, pers_rect):
         paused = True
@@ -128,7 +106,7 @@ def move_krest(krest):
         return
 
     if krest_bbox[2] < 1920:
-        window.after(10 , move_krest, krest) # и тут
+        window.after(10 , move_krest, krest)
     else:
         canvas.delete(krest)
         krest_count += 1
@@ -136,26 +114,22 @@ def move_krest(krest):
 image_image_4 = PhotoImage(file=relative_to_assets("image_2.png"))
 
 def create_and_move_image_2():
-    # Create the image at the left edge of the screen
-    image_2 = canvas.create_image(0, 540, image=image_image_4)  # Adjust y-coordinate as needed
+    image_2 = canvas.create_image(0, 540, image=image_image_4)  
     move_image_2(image_2)
 
-    # Reschedule the creation of the image every 10 seconds
     window.after(10000, create_and_move_image_2)
 
 def move_image_2(image):
-    canvas.move(image, 5, 0)  # Adjust speed as needed
+    canvas.move(image, 10, 0)
     x, y = canvas.coords(image)
     
     if x < canvas.winfo_width():
-        window.after(50, move_image_2, image)  # Adjust refresh rate as needed
+        window.after(1, move_image_2, image)  #
     else:
         canvas.delete(image)
 
-# Initialize the movement and creation of image_2
 create_and_move_image_2()
 def game_over():
-    # Выводим сообщение о завершении игры и предлагаем начать заново
     choice = tkinter.messagebox.askquestion("Игра закончена", "Хотите заново?")
     if choice == "yes":
         # Если выбрано "да", начинаем игру зановоя
@@ -163,7 +137,7 @@ def game_over():
         print('пока')
         window.destroy()
     else:
-        # Если выбрано "нет", закрываем окно
+        # Если выбрано "нет"?
         window.destroy()
 def check_collision(rect1, rect2):
     """Проверяет столкновение между двумя прямоугольниками"""
